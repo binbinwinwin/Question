cda2ff48a8f87b2ee563a7e14938e3e35532ce66
binbinwinwin <dah520kmm123456@gmail.com>
2021-11-29 23:00:21 +0800

    1

---
+++ test/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+      <card></card>
+      <ul>
+        <li class="a" v-for="item in data" :key="item.name">
+          <div>{{item}}</div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Card from './components/Card.vue';
+
+export default {
+  data(){
+    return{
+      data:[],
+    };
+  },
+  mounted(){
+  this.axios.get('https://mocki.io/v1/5f533945-c82e-462e-92a4-b4e21490b560')
+      .then((res)=>{
+        console.log(res);
+        this.data = res.data;
+      })
+  },
+  name: 'App',
+  components: {
+    HelloWorld,
+    Card
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ test/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="container">
+    <cardtest title="123"></cardtest>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue/dist/vue.esm.js';
+
+Vue.component('cardtest',{
+	props: ['title'],
+	template:`
+		<div>456
+		</div>
+	`,
+});
+
+export default {
+	data(){
+		return{
+		}
+	},
+}
+
+</script>
